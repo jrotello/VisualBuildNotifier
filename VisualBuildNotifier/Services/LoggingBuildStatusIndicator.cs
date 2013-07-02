7ebaaf23629bdd174d69e9e9941b143a6e0af464
Jason J. Rotello <jason@jrotello.com>
2013-07-01 19:39:05 -0500

    Forgot to add LoggingBuildStatusIndicator.cs

---
+++ VisualBuildNotifier/Services/LoggingBuildStatusIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.Client;
+using NLog;
+
+namespace VisualBuildNotifier.Services
+{
+    public class LoggingBuildStatusIndicator: BuildStatusIndicatorBase {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public override void ReportSuccess(IBuildDetail build) {
+            _logger.Trace("Build Succeeded: {0} by {1}", build.BuildNumber, build.RequestedFor);
+        }
+
+        public override void ReportFailure(IBuildDetail build) {
+            _logger.Trace("Build Failed: {0} by {1}", build.BuildNumber, build.RequestedFor);
+        }
+
+        public override void ReportInProgress(IBuildDetail build) {
+            _logger.Trace("Build In Progress: {0} by {1}", build.BuildNumber, build.RequestedFor);
+        }
+    }
+}
